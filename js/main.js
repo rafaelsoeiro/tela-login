4c5174133a685b4d849136b6ead6174b699ff8ea
rafaelsoeiro <103294767+rafaelsoeiro@users.noreply.github.com>
2022-06-27 22:18:52 -0300

    criando Js

---
+++ js/main.js
@@ -0,0 +1,4 @@
+let user= window.document.getElementsByClassName(user)
+        let pass= window.document.getElementsByClassName(password)
+
+        window.alert(`Usuario cadastrado com sucesso. Bem-vindo, ${user}`)
